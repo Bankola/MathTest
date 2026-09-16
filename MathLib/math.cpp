d8bc6555a0ea620b85814840571c18614996ca40
Trushkov. I <ivanyatrush@gmail.com>
2026-09-16 20:13:15 +0300

    Create test for MathLib

--- MathLib/math.cpp
+++ MathLib/math.cpp
@@ -1,43 +1,29 @@
 #include <random>
 #include <iostream>
 #include <ctime>
-#include "../MathLib/math.h"
+#include "../MathLib/math_lib.h"
 
 Task::Task() {
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
-	int num_1 = 1 + std::rand() % 10000; 
-	int num_2 = 1 + std::rand() % 10000;
-	char operation = 1 + std::rand() % 4; 
-
+	num_1 = 1 + std::rand() % 10000;   
+	num_2 = 1 + std::rand() % 10000;   
+	operation = 1 + std::rand() % 4;  
 	Result(operation);
 }
 
 Task::Task(int min, int max, char oper) {
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
-	int num_1 = std::rand() % (max - min + 1) + min;
-	int num_2 = std::rand() % (max - min + 1) + min;
+	num_1 = std::rand() % (max - min + 1) + min;   
+	num_2 = std::rand() % (max - min + 1) + min;   
 
-	if (oper != '\0') {
-		if (oper == '*') {
-			operation = 3;
-		}
-		else if (oper == '+') {
-			operation = 1;
-		}
-		else if (oper == '-') {
-			operation = 2;
-		}
-		else if (oper == '/') {
-			operation = 4;
-		}
-	}
-	else {
-		char operation = 1 + std::rand() % 4;
-	}
+	if (oper == '*')      operation = 3;
+	else if (oper == '+') operation = 1;
+	else if (oper == '-') operation = 2;
+	else if (oper == '/') operation = 4;
+	else                  operation = 1 + std::rand() % 4; 
 
 	Result(operation);
 }
-
 void Task::Result(char oper) {
 	switch (oper) {
 	case 1:
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,7 +1,74 @@
 #include "pch.h"
-#include "math.h"
+#include "math_lib.h"
 
-TEST(Mathlib, TestConstructor) {
-	Task t1();
+TEST(Task, DefaultNumbersInRange) {
+    for (int i = 0; i < 100; ++i) {
+        Task t;
+        EXPECT_GE(t.num_1, 1);
+        EXPECT_LE(t.num_1, 10000);
+        EXPECT_GE(t.num_2, 1);
+        EXPECT_LE(t.num_2, 10000);
+    }
+}
 
+TEST(Task, DefaultOperationInRange) {
+    for (int i = 0; i < 100; ++i) {
+        Task t;
+        EXPECT_GE(t.operation, 1);
+        EXPECT_LE(t.operation, 4);
+    }
+}
+
+TEST(Task, DefaultAnswerMatchesOperation) {
+    for (int i = 0; i < 100; ++i) {
+        Task t;
+        switch (t.operation) {
+        case 1: EXPECT_EQ(t.answer, t.num_1 + t.num_2); break;
+        case 2: EXPECT_EQ(t.answer, t.num_1 - t.num_2); break;
+        case 3: EXPECT_EQ(t.answer, t.num_1 * t.num_2); break;
+        case 4: EXPECT_EQ(t.answer, t.num_1 / t.num_2); break;
+        default: FAIL() << "unknown operation: " << (int)t.operation;
+        }
+    }
+}
+
+TEST(Task, RangeRespectsMinMax) {
+    for (int i = 0; i < 200; ++i) {
+        Task t(5, 10, '+');
+        EXPECT_GE(t.num_1, 5);
+        EXPECT_LE(t.num_1, 10);
+        EXPECT_GE(t.num_2, 5);
+        EXPECT_LE(t.num_2, 10);
+    }
+}
+
+TEST(Task, SingleValueRange) {
+    Task t(7, 7, '+');
+    EXPECT_EQ(t.num_1, 7);
+    EXPECT_EQ(t.num_2, 7);
+    EXPECT_EQ(t.answer, 14);
+}
+
+TEST(Task, PlusOperation) {
+    Task t(3, 8, '+');
+    EXPECT_EQ(t.operation, 1);
+    EXPECT_EQ(t.answer, t.num_1 + t.num_2);
+}
+
+TEST(Task, MinusOperation) {
+    Task t(3, 8, '-');
+    EXPECT_EQ(t.operation, 2);
+    EXPECT_EQ(t.answer, t.num_1 - t.num_2);
+}
+
+TEST(Task, MultiplyOperation) {
+    Task t(3, 8, '*');
+    EXPECT_EQ(t.operation, 3);
+    EXPECT_EQ(t.answer, t.num_1 * t.num_2);
+}
+
+TEST(Task, DivideOperation) {
+    Task t(1, 100, '/');
+    EXPECT_EQ(t.operation, 4);
+    EXPECT_EQ(t.answer, t.num_1 / t.num_2);
 }
