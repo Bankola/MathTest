db152ef2b98f2c74497431ff47cad80a4217d5a5
Trushkov. I <ivanyatrush@gmail.com>
2026-09-17 00:05:05 +0300

    Create MathTestClass(Without Tests!)

--- MathLib/math.cpp
+++ MathLib/math.cpp
@@ -1,105 +1,208 @@
-#include <random>
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
 #include "../MathLib/math_lib.h"
 
 Task::Task() {
-	std::srand(static_cast<unsigned int>(std::time(nullptr)));
-	num_1 = 1 + std::rand() % 10000;   
-	num_2 = 1 + std::rand() % 10000;   
-	operation = 1 + std::rand() % 4;  
-	Result(operation);
+    num_1 = 1 + std::rand() % 10000;
+    num_2 = 1 + std::rand() % 10000;
+    operation = static_cast<char>(1 + std::rand() % 4);
+    Result(operation);
 }
 
 Task::Task(int min, int max, char oper) {
-	std::srand(static_cast<unsigned int>(std::time(nullptr)));
-	num_1 = std::rand() % (max - min + 1) + min;   
-	num_2 = std::rand() % (max - min + 1) + min;   
+    if (max < min) {
+        std::swap(min, max);
+    }
 
-	if (oper == '*')      operation = 3;
-	else if (oper == '+') operation = 1;
-	else if (oper == '-') operation = 2;
-	else if (oper == '/') operation = 4;
-	else                  operation = 1 + std::rand() % 4; 
+    num_1 = std::rand() % (max - min + 1) + min;
+    num_2 = std::rand() % (max - min + 1) + min;
 
-	Result(operation);
+    if (oper == '*') {
+        operation = 3;
+    }
+    else if (oper == '+') {
+        operation = 1;
+    }
+    else if (oper == '-') {
+        operation = 2;
+    }
+    else if (oper == '/') {
+        operation = 4;
+    }
+    else {
+        operation = static_cast<char>(1 + std::rand() % 4);
+    }
+
+    Result(operation);
 }
 
 void Task::Result(char oper) {
-	switch (oper) {
-	case 1:
-		answer = num_1 + num_2;
-		break;
-	case 2:
-		answer = num_1 - num_2;
-		break;
-	case 3:
-		answer = num_1 * num_2;
-		break;
-	case 4:
-		answer = num_1 / num_2;
-		break;
-	}
+    switch (oper) {
+    case 1: 
+        answer = num_1 + num_2; 
+        break;
+    case 2: 
+        answer = num_1 - num_2; 
+        break;
+    case 3: 
+        answer = num_1 * num_2; 
+        break;
+    case 4:
+        if (num_2 == 0) { 
+            num_2 = 1; 
+        }
+        answer = static_cast<double>(num_1) / num_2;
+        break;
+    default:
+        answer = 0;
+        break;
+    }
 }
 
 void Task::print_question() const {
-	char sign;
-	switch (operation) {
-	case 1: sign = '+'; break;
-	case 2: sign = '-'; break;
-	case 3: sign = '*'; break;
-	case 4: sign = '/'; break;
-	default: sign = '?'; break;
-	}
-	std::cout << num_1 << " " << sign << " " << num_2 << " = ?";
+    char sign;
+    switch (operation) {
+    case 1: 
+        sign = '+'; 
+        break;
+    case 2: 
+        sign = '-'; 
+        break;
+    case 3: 
+        sign = '*'; 
+        break;
+    case 4: 
+        sign = '/'; 
+        break;
+    default: 
+        sign = '?'; 
+        break;
+    }
+    std::cout << num_1 << " " << sign << " " << num_2 << " = ?";
 }
 
 MathTest::MathTest(int questions_count) {
-	if (questions_count < 1) {
-		questions_count = 1;
-	}
+    if (questions_count < 1) {
+        questions_count = 1;
+    }
 
-	count = questions_count;
-	correct_count = 0;
+    count = questions_count;
+    correct_count = 0;
 
-	tasks = new Task[count];
-	user_answers = new int[count];
-	for (int i = 0; i < count; i++) {
-		user_answers[i] = 0;
-	}
+    tasks = new Task[count];
+    user_answers = new int[count]();
 }
 
 MathTest::MathTest(int questions_count, int min, int max) {
-	if (questions_count < 1) questions_count = 1;
-	if (max < min) std::swap(min, max);
+    if (questions_count < 1) {
+        questions_count = 1;
+    }
 
-	count = questions_count;
-	correct_count = 0;
+    if (max < min) {
+        std::swap(min, max);
+    }
 
-	tasks = new Task[count];
-	user_answers = new int[count]();
+    count = questions_count;
+    correct_count = 0;
 
-	for (int i = 0; i < count; ++i) {
-		tasks[i] = Task(min, max, '\0');  
-	}
+    tasks = new Task[count];
+    user_answers = new int[count]();
+
+    for (int i = 0; i < count; ++i) {
+        tasks[i] = Task(min, max, '\0');
+    }
 }
 
 MathTest::MathTest(int questions_count, int min, int max, char oper) {
-	if (questions_count < 1) questions_count = 1;
-	if (max < min) std::swap(min, max);
+    if (questions_count < 1) questions_count = 1;
+    if (max < min) std::swap(min, max);
 
-	count = questions_count;
-	correct_count = 0;
+    count = questions_count;
+    correct_count = 0;
 
-	tasks = new Task[count];
-	user_answers = new int[count]();
+    tasks = new Task[count];
+    user_answers = new int[count]();
 
-	for (int i = 0; i < count; ++i) {
-		tasks[i] = Task(min, max, oper);   
-	}
+    for (int i = 0; i < count; ++i) {
+        tasks[i] = Task(min, max, oper);
+    }
 }
 
 MathTest::~MathTest() {
-	delete[] tasks;
-	delete[] user_answers;
+    delete[] tasks;
+    delete[] user_answers;
+}
+
+bool MathTest::submit_answer(int index, int user_answer) {
+    if (index < 0 || index >= count) {
+        return false;
+    }
+
+    user_answers[index] = user_answer;
+
+    long expected = std::llround(tasks[index].answer);
+    bool ok = (static_cast<long>(user_answer) == expected);
+
+    if (ok) {
+        correct_count++;
+    }
+    return ok;
+}
+
+bool MathTest::is_correct(int index) const {
+    if (index < 0 || index >= count) {
+        return false;
+    }
+    long expected = std::llround(tasks[index].answer);
+    return static_cast<long>(user_answers[index]) == expected;
+}
+
+char MathTest::calculate_mark() const {
+    if (count == 0) return '2';
+
+    double percent = 100.0 * correct_count / count;
+
+    if (percent >= 90.0) return '5';
+    if (percent >= 75.0) return '4';
+    if (percent >= 60.0) return '3';
+    return '2';
+}
+
+void MathTest::run() {
+    std::cout << "=== Math Test (" << count << " questions) ===\n";
+
+    for (int i = 0; i < count; ++i) {
+        std::cout << "\n[" << (i + 1) << "/" << count << "] ";
+        tasks[i].print_question();
+        std::cout << "\n> ";
+
+        int user_input;
+        if (!(std::cin >> user_input)) {
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            user_input = 0;
+        }
+
+        submit_answer(i, user_input);
+    }
+
+    show_statistics();
+}
+
+void MathTest::show_statistics() const {
+    std::cout << "\n=== Statistics ===\n";
+
+    for (int i = 0; i < count; ++i) {
+        std::cout << (i + 1) << ") ";
+        tasks[i].print_question();
+        std::cout << "  True: " << static_cast<long>(tasks[i].answer)
+            << "  Your: " << user_answers[i]
+            << "  [" << (is_correct(i) ? '+' : '-') << "]\n";
+    }
+
+    std::cout << "\nTotal Result: " << correct_count << " / " << count
+        << " (mark: " << calculate_mark() << ")\n";
 }
