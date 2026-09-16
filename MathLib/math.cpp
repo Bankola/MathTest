5a8570305f2a9b63b699364c89a045f925f1bc90
Trushkov. I <ivanyatrush@gmail.com>
2026-09-16 22:22:22 +0300

    Added constructors for MathTest class

--- MathLib/math.cpp
+++ MathLib/math.cpp
@@ -24,6 +24,7 @@ Task::Task(int min, int max, char oper) {
 
 	Result(operation);
 }
+
 void Task::Result(char oper) {
 	switch (oper) {
 	case 1:
@@ -39,4 +40,66 @@ void Task::Result(char oper) {
 		answer = num_1 / num_2;
 		break;
 	}
+}
+
+void Task::print_question() const {
+	char sign;
+	switch (operation) {
+	case 1: sign = '+'; break;
+	case 2: sign = '-'; break;
+	case 3: sign = '*'; break;
+	case 4: sign = '/'; break;
+	default: sign = '?'; break;
+	}
+	std::cout << num_1 << " " << sign << " " << num_2 << " = ?";
+}
+
+MathTest::MathTest(int questions_count) {
+	if (questions_count < 1) {
+		questions_count = 1;
+	}
+
+	count = questions_count;
+	correct_count = 0;
+
+	tasks = new Task[count];
+	user_answers = new int[count];
+	for (int i = 0; i < count; i++) {
+		user_answers[i] = 0;
+	}
+}
+
+MathTest::MathTest(int questions_count, int min, int max) {
+	if (questions_count < 1) questions_count = 1;
+	if (max < min) std::swap(min, max);
+
+	count = questions_count;
+	correct_count = 0;
+
+	tasks = new Task[count];
+	user_answers = new int[count]();
+
+	for (int i = 0; i < count; ++i) {
+		tasks[i] = Task(min, max, '\0');  
+	}
+}
+
+MathTest::MathTest(int questions_count, int min, int max, char oper) {
+	if (questions_count < 1) questions_count = 1;
+	if (max < min) std::swap(min, max);
+
+	count = questions_count;
+	correct_count = 0;
+
+	tasks = new Task[count];
+	user_answers = new int[count]();
+
+	for (int i = 0; i < count; ++i) {
+		tasks[i] = Task(min, max, oper);   
+	}
+}
+
+MathTest::~MathTest() {
+	delete[] tasks;
+	delete[] user_answers;
 }
--- MathLib/math_lib.h
+++ MathLib/math_lib.h
@@ -9,6 +9,20 @@ struct Task {
 	Task();
 	Task(int min, int max, char oper = '\0');
 	void Result(char oper);
+
+	void print_question() const;
 };
 
-class MathTest;
+class MathTest {
+private:
+	Task* tasks;
+	int count;
+	int* user_answers;
+	int correct_count;
+public:
+	MathTest(int questions_count);
+	MathTest(int questions_count, int min, int max);
+	MathTest(int questions_count, int min, int max, char oper);
+	~MathTest();
+};
+
